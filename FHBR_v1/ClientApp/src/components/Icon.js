f79e98dbcfb38769b6f86efe359b8c856bea9b74
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-05-05 18:30:58 -0400

    Initial commit

---
+++ FHBR_v1/ClientApp/src/components/Icon.js
@@ -0,0 +1,16 @@
+﻿import React, { Component } from 'react';
+
+export class Icon extends Component {
+    static displayName = Icon.name;
+
+    render() {
+        return (
+            <span class="logo">
+                <a href="/">
+                    <img src="/white_logo.png" />
+                </a>
+            </span>
+        );
+    }
+}
+
