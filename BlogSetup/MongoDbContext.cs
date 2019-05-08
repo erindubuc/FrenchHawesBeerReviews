d5e6457ca7e2cc08296c4574e0caa0a21f929350
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-05-08 15:22:30 -0400

    Setting up Mongo Connection

---
+++ BlogSetup/MongoDbContext.cs
@@ -0,0 +1,47 @@
+﻿using MongoDB.Driver;
+using BlogSetup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogSetup
+{
+    public class MongoDbContext
+    {
+        public static string ConnectionString { get; set; }
+        public static string Database { get; set; }
+        public static bool IsSSL { get; set; }
+
+        private IMongoDatabase _database { get; }
+
+        public MongoDbContext()
+        {
+            try
+            {
+                MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(ConnectionString));
+
+                if (IsSSL)
+                {
+                    settings.SslSettings = new SslSettings { EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls };
+                }
+
+                var mongoClient = new MongoClient(settings);
+                _database = mongoClient.GetDatabase(Database);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Can't access db server. " + ex.Message);
+            }
+        }
+
+        public IMongoCollection<Post> Posts
+        {
+            get
+            {
+                return _database.GetCollection<Post>("Posts");
+            }
+        }
+    }
+}
