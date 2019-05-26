56359badb3a277cb1cf51c634253a84adca255dd
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-05-26 12:07:05 -0400

    Version with regular navbar, installed Google Youtube Nuget

---
+++ FHBR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FHBR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
