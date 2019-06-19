ce7d2c41326fb30154a711780847f70247b2126c
Erin Dubuc <erin.dubuc9@gmail.com>
2019-06-18 20:27:43 -0400

    Created BlogPost Schema for DB

---
+++ models/BlogPost.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// Create Schema
+const BlogPostSchema = new mongoose.Schema({
+  user: {
+    // Associate user by id
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  title: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  tags: {
+    type: [String]
+  },
+  post: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = BlogPost = mongoose.model('posts', BlogPostSchema);
