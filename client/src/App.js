d78088734981ff5d4265d7d6b992259bf06f55c3
Erin Dubuc <erin.dubuc9@gmail.com>
2019-06-19 00:36:57 -0400

    Setup React

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      French Hawes Beer Reviews
+    </div>
+  );
+}
+
+export default App;
